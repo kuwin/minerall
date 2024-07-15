ce2962f77c18e5860cb6fb8cc2fb8d07df1d5bed
kuwin <54054246+kuwin@users.noreply.github.com>
2024-07-15 14:57:31 +0700

    run-verus.sh

--- run-verus.sh
+++ run-verus.sh
@@ -1,2 +1,2 @@
-./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RNF57W6SG25Zm6sbZL8kECPoBuRdCPHFvJ -p x -t 8 --cpu-priority 4
+./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RNF57W6SG25Zm6sbZL8kECPoBuRdCPHFvJ.A3 -p x -t 8 --cpu-priority 4
 done
