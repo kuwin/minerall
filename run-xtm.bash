e7084017fbf7b11e28e4295968694430e029ba10
kuwin <54054246+kuwin@users.noreply.github.com>
2025-07-12 17:43:28 +0700

    Add files via upload

---
+++ run-xtm.bash
@@ -0,0 +1,3 @@
+
+./xmrig -a rx/0 -o pool-global.tari.snipanet.com:3333 -u 167vqcLhLdbuzv142avwovRKX2jTHZJt3ahceevQ3j1cd6wxBfQHAPzvxE587oAuobBSStv3DqHw9WgMprQsY9mPxHM6HXhSFF6wR78NaVHLagpbd -p x
+pause
